6c957b8dece0eb9222c4a660cffebeb0a01c9fb9
Joshua Chapman <joshua.chapman@joshuachp.dev>
2022-09-19 08:00:26 +0200

    feat: parse comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,60 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::borrow::Cow;
+
+use indexmap::IndexMap;
+use nom::{
+    character::complete::{char, line_ending, not_line_ending},
+    combinator::{map, recognize},
+    multi::many0_count,
+    sequence::pair,
+    IResult,
+};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Line<'a> {
+    Comment(Cow<'a, str>),
+    EmptyLine,
+    Group {
+        header: Cow<'a, str>,
+        entries: IndexMap<Key, Value>,
+    },
+}
+
+type Key = String;
+type Value = String;
+
+fn parse_comment(input: &str) -> IResult<&str, Line> {
+    map(recognize(pair(char('#'), not_line_ending)), |comment| {
+        Line::Comment(Cow::from(comment))
+    })(input)
+}
+
+fn parse_empty_line(input: &str) -> IResult<&str, Line> {
+    map(line_ending, |_| Line::EmptyLine)(input)
+}
+
+fn parse_group(input: &str) -> IResult<&str, Line> {
+    todo!()
+    // recognize(pair(, second))
+}
+
+fn parse_group_header(input: &str) -> IResult<&str, Cow<str>> {
+    todo!();
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn shoul_parse_comment() {
+        assert_eq!(
+            Ok(("\n", Line::Comment(Cow::from("# Code")))),
+            parse_comment("# Code\n")
+        )
+    }
+
+    #[test]
+    fn shoul_parse_empty_comment() {
+        assert_eq!(Ok(("", Line::Comment(Cow::from("#")))), parse_comment("#"))
     }
 }
